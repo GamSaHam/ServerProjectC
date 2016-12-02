4b3fa4dae9c3ab76b30d9714b113e9ad5ee5070c
GamSaHam <cleverones@naver.com>
2016-12-02 09:32:01 +0900

    npm install microtime

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 ﻿/*
     일자: 2016-12-01
-    내용: 스마트 헬멧을 위한 Node.js 서버
-   
+    내용: 스마트 헬멧을 위한 Node.js 서버   
 */
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var microtime = require('microtime');
+
+
 
 app.get('/', function (req, res) {
     res.sendfile('index.html');
@@ -22,11 +24,24 @@ io.on('connection', function (socket) {
 
     console.log('connection');
 
+    socket.on('sendPacket', function (data) {
+
+        var current_data =
+        {
+            s_current_ms: data.s_current_ms
+        };
+
+        console.log(current_data.s_current_ms);
+        console.log(microtime.now());
+
+
+
+    });
+
+
     socket.on('addUser', function () {
         console.log('add user');
         socketes.push(socket);
-
-
     });
 
     socket.on('musicPlay', function (data) {
@@ -34,18 +49,14 @@ io.on('connection', function (socket) {
         console.log('play the music');
 
         current_data =
-            {
-                s_player_index: data.s_player_index,
-                s_music_index: data.s_music_index
-            };
+        {
+            s_player_index: data.s_player_index,
+            s_music_index: data.s_music_index
+        };
 
         var index = parseInt(current_data.s_player_index);
 
         socketes[index].emit('onMusicPlay', data);
-
-
     });
-
-
 
 });
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/socket.io/index.d.ts" />
